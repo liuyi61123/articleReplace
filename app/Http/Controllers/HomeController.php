9d8b258adc25600d59b27cf33b9cc87bfa74e6ca
liuyi <475793342@qq.com>
2018-07-09 15:18:22 +0800

    修改生成图片的样式

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,12 +42,14 @@ class HomeController extends Controller
             <title>title</title>
             <style >
                 html,body{margin: 0px;padding: 0px;}
-                .title{background-color: red;text-align: center;color: #ffffff;font-size: 25px;}
+                .title{background-color: #C00000;text-align: center;color: #ffffff;font-size: 20px;font-weight: bold;}
                 .content{width:100%;}
                 table{width:100%;}
-                table tr td { border:1px solid red; }
-                .lable{color: red}
+                table tr td { border:1px solid #000000; }
+                .lable{text-align:center; color: #C00000}
                 .value{text-align:center}
+                .td_2{width: 50%;}
+                .td_6{width: 16.6%;}
             </style>
         </head>
         <body>
@@ -55,14 +57,14 @@ class HomeController extends Controller
                 <span>车辆贷款审批表</span>
             </div>
             <div class="content">
-                <table border="1" cellspacing="0">
+                <table border="1" cellspacing="0" bordercolor="#a0c6e5" style="border-collapse:collapse;">
                     <tr>
-                        <td class="lable">客户姓名</td>
-                        <td class="value">王先生</td>
-                        <td class="lable">户口</td>
-                        <td class="va">上海</td>
-                        <td class="lable">家庭状况</td>
-                        <td class="value">已婚</td>
+                        <td class="lable td_6">客户姓名</td>
+                        <td class="value td_6">王先生</td>
+                        <td class="lable td_6">户口</td>
+                        <td class="value td_6">上海</td>
+                        <td class="lable td_6">家庭状况</td>
+                        <td class="value td_6">已婚</td>
                     </tr>
                     <tr>
                         <td class="lable">借款金额</td>
@@ -73,12 +75,18 @@ class HomeController extends Controller
                         <td class="value">等额本息</td>
                     </tr>
                     <tr>
-                        <td class="lable">客户姓名</td>
-                        <td class="value">王先生</td>
-                        <td class="lable">户口</td>
-                        <td class="va">上海</td>
-                        <td class="lable">家庭状况</td>
-                        <td class="value">已婚</td>
+                        <td class="lable">车辆型号</td>
+                        <td class="value" colspan="3">本田雅阁 2015款 2.4 VIP版</td>
+                        <td class="lable">拍照</td>
+                        <td class="value">沪大牌</td>
+                    </tr>
+                    <tr>
+                        <td class="lable">出厂日期</td>
+                        <td class="value">2015年11月</td>
+                        <td class="lable">上牌日期</td>
+                        <td class="value">2016年4月</td>
+                        <td class="lable">全款买车</td>
+                        <td class="value">是</td>
                     </tr>
                 </table>
             </div>
@@ -87,24 +95,86 @@ class HomeController extends Controller
                 <span>初审评估</span>
             </div>
             <div class="content">
-                <table border="1" cellspacing="0">
+                <table border="1" cellspacing="0" bordercolor="#a0c6e5" style="border-collapse:collapse;">
                     <tr>
-                        <td class="value" colspan="4">提额条件</td>
-                        <td class="value">附加条件</td>
-                        <td colspan="1"></td>
+                        <td class="value td_2">提额条件</td>
+                        <td class="value td_2">附加条件</td>
                     </tr>
                     <tr>
-                        <td colspan="4">1车辆本身评估价格</td>
-                        <td colspan="1">加9万</td>
-                        <td></td>
+                        <td class="value">汽车本身评估价格</td>
+                        <td class="lable">加15万</td>
                     </tr>
                     <tr>
-                        <td colspan="4">1车辆本身评估价格</td>
-                        <td colspan="1">加9万</td>
-                        <td colspan="1"></td>
+                        <td class="value">汽车拍照为沪大牌</td>
+                        <td class="lable">加8万</td>
+                    </tr>
+                    <tr>
+                        <td class="value">登记证,身份证,行驶证,保险单,备用钥匙</td>
+                        <td class="lable">原件齐全</td>
+                    </tr>
+                    <tr>
+                        <td class="value">借款人为上海人</td>
+                        <td class="lable">可加额度</td>
+                    </tr>
+                    <tr>
+                        <td class="value">借款人在上海有房产</td>
+                        <td class="lable">可加额度</td>
+                    </tr>
+                    <tr>
+                        <td class="value">汽车保养良好,行驶少于10万公里</td>
+                        <td class="lable">可加额度</td>
+                    </tr>
+                    <tr>
+                        <td class="value">借款人征信良好流水正常</td>
+                        <td class="lable">可加额度</td>
                     </tr>
                 </table>
             </div>
+
+            <div class="title">
+                <span>评估结果</span>
+            </div>
+            <div class="content">
+                <table border="1" cellspacing="0" bordercolor="#a0c6e5" style="border-collapse:collapse;">
+                    <tr>
+                        <td class="lable td_6">风控意见</td>
+                        <td class="value td_6">通过</td>
+                        <td class="lable td_6">额度评估</td>
+                        <td class="value td_6">25万</td>
+                        <td class="lable td_6">客户需要</td>
+                        <td class="value td_6">23万</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="title">
+                <span>23万 还款计算表</span>
+            </div>
+            <div class="content">
+                <table border="1" cellspacing="0" bordercolor="#a0c6e5" style="border-collapse:collapse;">
+                    <tr>
+                        <td class="lable td_6">借款金额</td>
+                        <td class="value td_6">230000</td>
+                        <td class="lable td_6">借款利率</td>
+                        <td class="value td_6">0.95</td>
+                        <td class="lable td_6">贷款时间</td>
+                        <td class="value td_6">12个月</td>
+                    </tr>
+                    <tr>
+                        <td class="lable">总利息</td>
+                        <td class="value">26220元</td>
+                        <td class="lable">每月利息</td>
+                        <td class="value">2185元</td>
+                        <td class="lable">折合每日</td>
+                        <td class="value">72元</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="title">
+                <span>注:以上信息仅供参考</span>
+            </div>
+
         </body>
         </html>
 

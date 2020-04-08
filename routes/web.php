2e4d0b309694c238fd129245979742d19c565f98
liuyi <475793342@qq.com>
2020-04-08 13:33:47 +0800

    3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'Auth\LoginController@login');
-Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Auth::routes();
+// Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+// Route::post('/login', 'Auth\LoginController@login');
+// Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/', 'ArticlesController@index')->name('home');
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
